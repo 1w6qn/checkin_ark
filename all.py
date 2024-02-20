978086882b34ef0fdf46d5f8d135807e019e41dc
1w6qn <61302646+1w6qn@users.noreply.github.com>
2024-02-20 16:24:02 +0800

    Update all.py

--- all.py
+++ all.py
@@ -1,20 +1,21 @@
 import hashlib
 import hmac
 import json
+import os
 import random
 import string
 from time import time
-import os
+
 import requests
 
 temp = {}
-phone = os.environ["PHONE"]
+phone=os.environ["PHONE"]
 pwd=os.environ["PWD"]
-def GetMd5(src):
+
+def get_md5(src):
     m1 = hashlib.md5()
     m1.update(src.encode('utf-8'))
     return m1.hexdigest()
-
 
 def u8_sign(data):
     sign = hmac.new('91240f70c09a08a6bc72af1a5c8d4670'.encode(), data.encode(), hashlib.sha1)
@@ -32,17 +33,18 @@ def post(cgi, data, token):
 
 resv = requests.get("https://ak-conf.hypergryph.com/config/prod/official/Android/version").json()
 
-token1 = requests.post(
-    "https://as.hypergryph.com/user/auth/v1/token_by_phone_password",
-    json={"phone": phone, "password": pwd}
-).json()["data"]["token"]
+res = requests.post("https://as.hypergryph.com/user/auth/v1/token_by_phone_password",json={"phone": phone, "password": pwd}).json()
+token1 = res["data"]["token"]
+
+
 token2 = requests.post(
     "https://as.hypergryph.com/user/oauth2/v2/grant",
     json={"token": token1, "appCode": "7318def77669979d", "type": 0}
 ).json()["data"]["code"]
-device_id1 = GetMd5(''.join(random.choices(string.ascii_letters + string.digits, k=12)))
+
+device_id1 = get_md5(''.join(random.choices(string.ascii_letters + string.digits, k=12)))
 device_id2 = '85' + ''.join(random.choices(string.digits, k=13))
-device_id3 = GetMd5(''.join(random.choices(string.ascii_letters + string.digits, k=12)))
+device_id3 = get_md5(''.join(random.choices(string.ascii_letters + string.digits, k=12)))
 get_token_req = {
     "appId": "1",
     "channelId": "1",
@@ -115,7 +117,7 @@ if act := player_data["activity"]["BLESS_ONLY"]:
 # pray only
 if act := player_data["activity"]["PRAY_ONLY"]:
     for k, v in act.items():
-        post('/activity/prayOnly/getReward', {"activityId": k, "prayArray": list(range(1,v["prayDaily"]+1))}, token)
+        post('/activity/prayOnly/getReward', {"activityId": k, "prayArray": list(range(1, v["prayDaily"] + 1))}, token)
 
 # mail
 mail_list = post('/mail/getMetaInfoList', {"from": int(time())}, token)['result']
@@ -129,13 +131,6 @@ for i in mail_list:
 if not (norm and sys):
     post('/mail/receiveAllMail', {"mailIdList": norm, "sysMailIdList": sys}, token)
 print("mail finish")
-# social
-if player_data["social"]["yesterdayReward"]["canReceive"]:
-    post("/social/receiveSocialPoint", {}, token)
-
-# TODO buy social good
-
-print("social finish")
 
 
 # recruit
@@ -269,6 +264,8 @@ for i in range(0, 4):
     if slot['state'] == 2:
         post('/gacha/finishNormalGacha', {"slotId": str(i)}, token)
     tag_list, special_tag_id, duration = select_tag(slot['tags'])
+    if (tag_list == []) and player_data["building"]["rooms"]["HIRE"]["slot_23"]["refreshCount"]:
+        post('/gacha/refreshTags', {"slotId": i}, token)
     if not (special_tag_id == 11):
         post('/gacha/normalGacha', {
             "slotId": str(i), "tagList": tag_list, "specialTagId": special_tag_id, "duration": duration
@@ -277,18 +274,40 @@ for i in range(0, 4):
 print("recruit finish")
 # building
 post("/building/gainAllIntimacy", {}, token)
-"""
 
-if player_data["building"]["rooms"]["MEETING"]["slot_36"]["socialReward"]["daily"]:
+if player_data["building"]["rooms"]["MEETING"]["slot_36"]["dailyReward"]:
     post('/building/getDailyClue', {}, token)
+
+t = []
 if player_data["building"]["rooms"]["MEETING"]["slot_36"]["socialReward"]["daily"]:
-    post('/building/getMeetingroomReward', {"type": [0, 1]}, token)
-"""
+    t.append(0)
+if player_data["building"]["rooms"]["MEETING"]["slot_36"]["socialReward"]["search"]:
+    t.append(1)
+if t:
+    post('/building/getMeetingroomReward', {"type": t}, token)
+if len(player_data["building"]["rooms"]["MEETING"]["slot_36"]["board"]) == 7:
+    post('/building/startInfoShare', {}, token)
 post('/building/settleManufacture',
      {"roomSlotIdList": list(player_data["building"]["rooms"]["MANUFACTURE"].keys()), "supplement": 1}, token)
 post("/building/deliveryBatchOrder", {"slotList": list(player_data["building"]["rooms"]["TRADING"].keys())}, token)
 print("building finish")
-# campaignv2
+# social
+friend_list=post("/social/getSortListInfo", {"type":1,"sortKeyList":["level","infoShare"],"param":{}}, token)["result"]
+for index,friend in enumerate(friend_list):
+    if index>10:break
+    post("/building/visitBuilding", {"friendId": friend["uid"]}, token)
+if player_data["social"]["yesterdayReward"]["canReceive"]:
+    post("/social/receiveSocialPoint", {}, token)
+
+good_list = post("/shop/getSocialGoodList", {}, token)["goodList"]
+for good in good_list:
+    if player_data["status"]["socialPoint"] >= good["price"]:
+        post("/shop/buySocialGood", {"goodId": good['goodId'], "count": 1}, token)
+        player_data["status"]["socialPoint"] -= good["price"]
+
+print("social finish")
+
+# campaignV2
 stage_id = player_data["campaignsV2"]["open"]["rotate"]
 inst_id = 0
 for k, v in player_data["consumable"]["EXTERMINATION_AGENT"].items():
@@ -296,8 +315,9 @@ for k, v in player_data["consumable"]["EXTERMINATION_AGENT"].items():
         inst_id = int(k)
         break
 
-if (player_data["campaignsV2"]["sweepMaxKills"].get(stage_id, 0) == 400) and inst_id and (player_data["status"]["ap"] >= 25)\
-        and (player_data["campaignsV2"]["campaignCurrentFee"]<player_data["campaignsV2"]["campaignTotalFee"]):
+if (player_data["campaignsV2"]["sweepMaxKills"].get(stage_id, 0) == 400) and inst_id and (
+        player_data["status"]["ap"] >= 25) \
+        and (player_data["campaignsV2"]["campaignCurrentFee"] < player_data["campaignsV2"]["campaignTotalFee"]):
     post("/campaignV2/battleSweep", {"stageId": stage_id, "itemId": "EXTERMINATION_AGENT", "instId": inst_id}, token)
 print("campaign finish")
 # mission
